16b61ae0cc1147395c2450d8db4f8d8f105251d0
skaftisveins <skaftisveins@gmail.com>
2020-01-25 16:28:45 +0000

    Added Stripe payment intergration Modified Checkout page Added new favicon.ico

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -9,6 +9,7 @@ import {
   selectCartTotal
 } from '../../redux/cart/cartSelectors';
 import './Checkout.styles.scss';
+import StripeButton from '../../components/stripe-button/Stripe-button';
 
 const CheckoutPage = ({ cartItems, total }) => {
   return (
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className='total'>
         <span>TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVC: 123
+      </div>
+      <StripeButton price={total} />
     </div>
   );
 };

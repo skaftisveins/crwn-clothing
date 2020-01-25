f3fceae0ead5eac23891e99e46978d3c419992d5
skaftisveins <skaftisveins@gmail.com>
2020-01-25 23:22:38 +0000

    homepage component issues..

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/userActions';
+import { selectCurrentUser } from './redux/user/userSelector';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
-import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
+import HomePage from './pages/homepage/Homepage';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/Sign-in-and-sign-up';
 import CheckoutPage from './pages/checkout/Checkout';
 
 import Header from './components/header/Header';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/userActions';
-import { selectCurrentUser } from './redux/user/userSelector';
+
+
+import './App.css';
 class App extends React.Component {
   unsubscribeFromAuth = null;
 

c61874e6af83b004155363a9c8d1c625413de1e0
skaftisveins <skaftisveins@gmail.com>
2020-01-26 00:20:26 +0000

    Renamed all components and pages and fixed related imports..

---
+++ src/components/header/header.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { auth } from '../../firebase/firebase.utils';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
+import { selectCartHidden } from '../../redux/cart/cartSelectors';
+import { selectCurrentUser } from '../../redux/user/userSelector';
+
+// #region Styled components...
+const StyledHeader = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+
+  .logo-container {
+    height: 100%;
+    width: 70px;
+    padding: 25px;
+  }
+
+  .options {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    .option {
+      padding: 10px 15px;
+      cursor: pointer;
+    }
+  }
+`;
+// #endregion
+
+const Header = ({ currentUser, hidden }) => {
+  return (
+    <StyledHeader>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
+      </div>
+      {hidden ? null : <CartDropdown />}
+    </StyledHeader>
+  );
+};
+
+const mapStateToProps = state =>
+  createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+  });
+
+export default connect(mapStateToProps)(Header);
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
+
+import './sign-in-and-sign-up.styles.css';
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;

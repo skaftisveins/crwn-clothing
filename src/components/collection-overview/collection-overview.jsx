c3d398c53fe9b705e3d8f4ef9ef774819f4bccf2
skaftisveins <skaftisveins@gmail.com>
2020-02-06 00:11:48 +0000

    Added HOC (spinner)

--- src/components/collection-overview/collection-overview.jsx
+++ src/components/collection-overview/collection-overview.jsx
@@ -4,15 +4,15 @@ import { createStructuredSelector } from 'reselect';
 import { selectCollectionsForPreview } from '../../redux/shop/shopSelector';
 import CollectionPreview from '../collection-preview/collection-preview';
 
-import { CollectionOverviewContainer } from './collection-overview.styles';
+import { CollectionsOverviewContainer } from './collection-overview.styles';
 
-const CollectionOverview = ({ collections }) => {
+const CollectionsOverview = ({ collections }) => {
   return (
-    <CollectionOverviewContainer>
+    <CollectionsOverviewContainer>
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </CollectionOverviewContainer>
+    </CollectionsOverviewContainer>
   );
 };
 
@@ -20,4 +20,4 @@ const mapStateToProps = createStructuredSelector({
   collections: selectCollectionsForPreview
 });
 
-export default connect(mapStateToProps, null)(CollectionOverview);
+export default connect(mapStateToProps, null)(CollectionsOverview);
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionOverview from '../../components/collection-overview/collection-overview';
+import CollectionsOverview from '../../components/collection-overview/collection-overview';
 import CollectionPage from '../collection/collection';
+import WithSpinner from '../../components/with-spinner/with-spinner';
 
 import {
   firestore,
@@ -12,28 +13,56 @@ import {
 
 import { updateCollections } from '../../redux/shop/shopActions';
 
-const ShopPage = ({ match, updateCollections }) => {
-  useEffect(() => {
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React {
+  state = {
+    loading: true
+  };
+
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
-  });
+  }
 
-  console.log(match);
-  return (
-    <div className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   updateCollections: collectionsMap =>
     dispatch(updateCollections(collectionsMap))
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
